e35c49dd59d6cb516ac3474ca1b2523d9a93ec35
Kishore Kumar Balakrishnan <kishorekumar.balakrishnan@tatacommunications.com>
2021-10-27 09:08:54 +0530

    #master - implementation for context provider and consumer

--- udemy-app/src/components/ContextApiApp.js
+++ udemy-app/src/components/ContextApiApp.js
@@ -3,15 +3,18 @@ import ContextNavbar from "./ContextNavbar";
 import TodoList from "./TodoList";
 import "../css/ContextApiApp.css";
 import ThemeContextProvider from "../contexts/ThemeContext";
+import AuthContextProvider from "../contexts/AuthContext";
 
 function ContextApiApp() {
     return (
         <div className="App">
             <div className="ui raised very padded text container segment">
-                <ThemeContextProvider>
-                    <ContextNavbar></ContextNavbar>
-                    <TodoList></TodoList>
-                </ThemeContextProvider>
+                <AuthContextProvider>
+                    <ThemeContextProvider>
+                        <ContextNavbar></ContextNavbar>
+                        <TodoList></TodoList>
+                    </ThemeContextProvider>
+                </AuthContextProvider>
             </div>
         </div>
     )
--- udemy-app/src/components/TodoList.js
+++ udemy-app/src/components/TodoList.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class TodoList extends React.Component {
+    static contextType = ThemeContext;
+
     render() {
+        const { isDarkTheme, lightTheme, darkTheme, changeTheme } = this.context;
+        const theme = isDarkTheme ? darkTheme : lightTheme;
+
         return (
-            <div className="ui list">
+            <div style={{ backgroundColor: theme.background, color: theme.text, height: '160px', textAlign: 'center' }}>
                 <p className="item"> Plan the family trip </p>
                 <p className="item"> Go for shopping for dinner </p>
                 <p className="item"> Go for a walk </p>
+
+                <button className="ui button primary" onClick={changeTheme}> Change Theme </button>
             </div>
         )
     }
